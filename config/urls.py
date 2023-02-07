064049008af0f619520f7b0278840ce94b9317eb
gkiserpong <gkiserpongit@gmail.com>
2023-02-07 08:37:22 +0700

    Meskinya

--- config/urls.py
+++ config/urls.py
@@ -9,18 +9,15 @@ admin.site.site_title = 'Saltis WebFilter'
 admin.site.index_title = 'Saltis WebFilter'
 
 urlpatterns = [
-    path('/', admin.site.urls),
     path('admin/', admin.site.urls),
     path('webfilters/', include('webfilters.urls')),
 ]
 
-'''
 if settings.DEBUG:
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
     urlpatterns += staticfiles_urlpatterns() # tell gunicorn where static files are in dev mode
-    urlpatterns += static(settings.MEDIA_URL + 'media/', document_root=os.path.join(settings.MEDIA_ROOT, 'media'))
-    urlpatterns += [
-        path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'myapp/images/favicon.ico'))
-    ]
-'''
+    #urlpatterns += static(settings.MEDIA_URL + 'media/', document_root=os.path.join(settings.MEDIA_ROOT, 'media'))
+    #urlpatterns += [
+    #    path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'myapp/images/favicon.ico'))
+    #]
